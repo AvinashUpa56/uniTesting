d18b729398c859084fe372ff55c23f39a8095180
Rahul Lama <rahullama95@gmail.com>
2022-09-12 16:35:08 +0530

    Group test addded

--- test/email_password_test.dart
+++ test/email_password_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+//import 'package:test/scaffolding.dart';
 import 'package:unitesting/email_password_validator.dart';
 
 void main() {
@@ -31,4 +32,39 @@ void main() {
     var result = FieldValidator.validatePassword('12345678');
     expect(result, null);
   });
+
+  group('Email Test', (){
+    test('Empty Email Test', () {
+      var result = FieldValidator.validateEmail('');
+      expect(result, 'Enter Email');
+    });
+
+    test('Invalid Email Test', () {
+      var result = FieldValidator.validateEmail(' ');
+      expect(result, 'Enter Valid Email');
+    });
+
+    test('Valid Email Test', () {
+      var result = FieldValidator.validateEmail('test12@gmail.com');
+      expect(result, null);
+    });
+  });
+
+  group('Password Test', (){
+    test('Empty Password Test', () {
+      var result = FieldValidator.validatePassword('');
+      expect(result, 'Enter Password');
+    });
+
+    test('Invalid Password Test', () {
+      var result = FieldValidator.validatePassword('123');
+      expect(result, 'Password must be more than 6 charater');
+    });
+
+    test('Valid Password Test', () {
+      var result = FieldValidator.validatePassword('12345678');
+      expect(result, null);
+    });
+  });
+
 }
